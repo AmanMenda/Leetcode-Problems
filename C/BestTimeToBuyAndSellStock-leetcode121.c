fb6db2e9c568feb8475642f8e22967cec006b1b5
Aman Menda <aman.menda@epitech.eu>
2022-08-04 14:17:26 +0100

    Leetcode121

---
+++ C/BestTimeToBuyAndSellStock-leetcode121.c
@@ -0,0 +1,13 @@
+int maxProfit(int *prices, int pricesSize){
+    int l = 0, r = 1;
+    int maxP = 0;
+    
+    while (r < pricesSize) {
+        if (prices[r] > prices[l])
+            maxP = prices[r] - prices[l] > maxP ? prices[r] - prices[l] : maxP;
+        else
+            l = r;
+        r++;
+    }
+    return maxP;
+}
